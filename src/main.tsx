4b31a6f071be616478f83b2e9891c33b982482e0
Hieu2M <thuonghieutk1996@gmail.com>
2024-12-06 15:13:34 -0800

    update refresh function

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Handle stored route on page load
+const path = window.localStorage.getItem('path');
+if (path) {
+    window.localStorage.removeItem('path');
+    window.history.replaceState(null, '', path);
+}
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
